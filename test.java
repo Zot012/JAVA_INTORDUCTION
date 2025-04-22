4301ad2e60d27e5f86ab3bc88cadfcf30d23947a
hugo0329 <hugo940329@gmail.com>
2025-04-22 21:39:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257013\"\n姓名：\"[林維祥]\"");
         System.out.println("\"學號：\"01257005\"\n姓名：\"[薛靖暘]\"");
+        System.out.println("\"學號：\"01257023\"\n姓名：\"[陳鵬宇]\"");
     }
 }
