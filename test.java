ddbf568f2e4dc47234a5567b42de59d67b4c3a31
Zot012 <151005549+Zot012@users.noreply.github.com>
2025-04-22 21:34:19 +0800

    feat.完成骨幹&組長資訊

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257013\"\n姓名：\"[林維祥]\"");
+    }
+}
